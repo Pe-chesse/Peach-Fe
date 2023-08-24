c7de29bd31868cf1db506d0e3a28b4bb484ab6c6
Seungil Jung <jungsi1217@naver.com>
2023-08-24 21:36:23 +0900

    feat(module): feat register module, verification with sending email

--- js/emailreg.js
+++ js/emailreg.js
@@ -1,39 +1,40 @@
-import { initializeApp } from 'firebase/app';
-import dotenv from 'dotenv'
-import { createUserWithEmailAndPassword, getAuth, sendEmailVerification } from 'firebase/auth';
-dotenv.config();
-
-
+require('dotenv').congif;
 const firebaseCongif = {
     apiKey: process.env.FIREBASE_KEY,
     authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env,
     projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID,
+    mesurementid : process.env.MEASUREMENT_ID,
 };
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+
 
 const app = initializeApp(firebaseCongif);
 
-const joinWithVerification = async (email,password) => {
-    try {
-        const auth = getAuth();
-        await createUserWithEmailAndPassword(auth, email, password);
-        await sendEmailVerification(auth.currentUser);
-        alert(authMessage['auth/post-email-verification-mail']);
-    }
-    catch({code, message}) {
-        alert(errorMessage[code])
-    }
-}
-
-
 document.querySelector('.next-btn').addEventListener('click',(e)=>{
-    let email = document.querySelector('#email').value;
-    let password = document.querySelector('#password').value; 
-    console.log(email)
-    console.log(password)
     e.preventDefault()
-    joinWithVerification(email, password)
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+    const auth = getAuth();
+    console.log(email, password)
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((res)=>{
+        console.log('회원가입 메일 전송 완료')
+        console.log(res)
+        sendEmailVerification(auth.currentUser)
+        if(auth.currentUser.emailVerified == true){
+            console.log('이메일 인증 성공')
+            window.location.href='/login.html'
+        }
+    })
+    .catch((err)=>{
+        console.log('회원가입 실패')
+        const errorCode = err.code;
+        const errormMessage = err.message;
+    })
 })
