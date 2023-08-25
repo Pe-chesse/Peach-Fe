072ec357fe23cad3d0a551bb8d83780fa8740694
Seungil Jung <jungsi1217@naver.com>
2023-08-25 17:28:50 +0900

    feat(module): produce firebase regist module and authenticate with email

--- js/emailreg.js
+++ js/emailreg.js
@@ -1,12 +1,12 @@
-require('dotenv').congif;
+
 const firebaseCongif = {
-    apiKey: process.env.FIREBASE_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    mesurementid : process.env.MEASUREMENT_ID,
+    apiKey: config.FIREBASE_KEY,
+    authDomain: config.AUTH_DOMAIN,
+    projectId: config.PROJECT_ID,
+    storageBucket: config.STORAGE_BUCKET,
+    messagingSenderId: config.MESSAGING_SENDER_ID,
+    appId: config.APP_ID,
+    mesurementid : config.MEASUREMENT_ID,
 };
 
 
@@ -20,21 +20,37 @@ document.querySelector('.next-btn').addEventListener('click',(e)=>{
     e.preventDefault()
     let email = document.querySelector('#email').value;
     let password = document.querySelector('#password').value;
+    const back = document.querySelector('.back-black');
+    const pop = document.querySelector('.popup');
+    
     const auth = getAuth();
-    console.log(email, password)
+
     createUserWithEmailAndPassword(auth, email, password)
     .then((res)=>{
-        console.log('회원가입 메일 전송 완료')
-        console.log(res)
-        sendEmailVerification(auth.currentUser)
-        if(auth.currentUser.emailVerified == true){
-            console.log('이메일 인증 성공')
-            window.location.href='/login.html'
+        sendEmailVerification(auth.currentUser);
+        if(sendEmailVerification){
+            back.classList.add('dibl')
+            pop.classList.add('dibl')
+        }
+        else{
+            console.log('error: style apply failed')
         }
     })
-    .catch((err)=>{
-        console.log('회원가입 실패')
-        const errorCode = err.code;
-        const errormMessage = err.message;
+    .then((res)=>{
+        setTimeout(() => {
+            back.classList.add('op07')
+            pop.classList.add('op1')
+        }, 300);
     })
+    .catch((err)=>{
+        console.log('error')
+        const errorCode = err.code;
+        console.log(errorCode)
+        const errormMessage = err.message;
+        console.log(errormMessage)
+    })
+})
+
+document.querySelector('.check-btn').addEventListener('click',(e)=>{
+    window.location.href='/loginpage.html'
 })
