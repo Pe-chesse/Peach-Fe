c7d313731927669c3ceb7b7147c55540bc57bd39
kimyeoju <yeojoo0031@gmail.com>
2023-08-28 14:03:23 +0900

    feat(search) add search result processing features

--- js/search.js
+++ js/search.js
@@ -4,6 +4,14 @@ prevButton.addEventListener('click', () => {
     window.location.href = 'homepage.html';
 });
 
+// 검색 결과 처리
+const userSearchInput = document.getElementById('user-search');
+userSearchInput.addEventListener('input', async (event) => {
+    const searchQuery = event.target.value;
+    const response = await fetch(`http://127.0.0.1:8000/api/v1/post/search/?search_query=${searchQuery}`);
+    const data = await response.json();
+});
+
 // tab-menu
 const tabMenuElements = document.querySelectorAll('.tab-menu-elements li');
 
