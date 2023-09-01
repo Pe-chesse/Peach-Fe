5a884996efb13ac7cd8f74ea56a87479a6e10aaf
kimyeoju <yeojoo0031@gmail.com>
2023-09-01 16:07:20 +0900

    feat(myprofile) load my profile page posts

--- js/homepage_tabmenu.js
+++ js/homepage_tabmenu.js
@@ -5,15 +5,3 @@ if(document.querySelector('.wrapper').classList.contains('logo')){
         window.location.href = './searchpage.html';
     });
 }
-
-// tab-menu
-// const tabMenuElements = document.querySelectorAll('.tab-menu-elements li');
-
-// tabMenuElements.forEach(element => {
-//     element.addEventListener('click', () => {
-//     tabMenuElements.forEach(otherElement => {
-//     otherElement.classList.remove('on');
-//     });
-//     element.classList.add('on');
-// });
-// });
