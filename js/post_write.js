9eff6086b31919e10ee4133b0f4158f8de20c5d1
Deeklming <deeklming6850@gmail.com>
2023-09-01 16:18:47 +0900

    feat(post): success post write

--- js/post_write.js
+++ js/post_write.js
@@ -11,52 +11,52 @@ const firebaseConfig = {
   mesurementid : config.MEASUREMENT_ID,
 };
 
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth();
-// const baseurl = "http://3.37.239.49/api/v1/";
-// let idtoken;
-// let myInfo;
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const baseurl = "http://3.37.239.49/api/v1/";
+let idtoken;
+let myInfo;
 
-// onAuthStateChanged(auth, async (user)=>{
-//   if(user){
-//     idtoken = await user.getIdToken();
+onAuthStateChanged(auth, async (user)=>{
+  if(user){
+    idtoken = await user.getIdToken();
 
-//     const contentPost = async ()=>{
-//       const result = await fetch(`${baseurl}account/verify/`, {
-//           method : "GET",
-//           headers : {
-//               Authorization: `bearer ${idtoken}`,
-//           }
-//       })
-//       .then((res)=>{
-//           return res.json();
-//       })
-//       .then((res)=>{
-//           myInfo = res;
-//           document.querySelector(".profile_img").src = res.image_url;
-//           console.log(res);
-//       })
-//       .catch((err)=>{
-//           console.error(err);
-//       })
-//     }
-//     contentPost();
-//   }
-// })
+    const contentPost = async ()=>{
+      const result = await fetch(`${baseurl}account/verify/`, {
+          method : "GET",
+          headers : {
+              Authorization: `bearer ${idtoken}`,
+          }
+      })
+      .then((res)=>{
+          return res.json();
+      })
+      .then((res)=>{
+          myInfo = res;
+          document.querySelector(".profile_img").src = res.image_url;
+          console.log(res);
+      })
+      .catch((err)=>{
+          console.error(err);
+      })
+    }
+    contentPost();
+  }
+})
 
 const textArea = document.querySelector(".upload_post");
 const uploadImgBtn = document.querySelector(".upload_input");
 const imgContainer = document.querySelector(".img-container");
-let imageKeys;
+let imageKeys = [];
 
 uploadImgBtn.addEventListener("change", async function() {
   const f = this.files;
-
+  
   let formData = new FormData();
   for(let i=0; i<f.length; i++){
     formData.append('files', f[i]);
   }
-  console.log(formData.getAll('files'));
+  // console.log(formData.getAll('files'));
 
   if (f) {
     const result = await fetch(`${baseurl}bucket/media/`, {
@@ -66,12 +66,15 @@ uploadImgBtn.addEventListener("change", async function() {
       },
       body: formData,
     })
-      .then(async (res) => {
+      .then(async (res) => {//이미지 요소 및 키 추가
         const data = await res.json();
-        // console.log(data);
-        imageKeys = data.image_keys;
-        console.log(imageKey);
-        // profileImgRenderer.src = `${baseurl}bucket/media/?key=${imageKey}`;
+        let tmp = imageKeys.length;
+        imageKeys.push(...data.image_keys);
+        console.log(imageKeys);
+        for(let i=0; i<imageKeys.length; i++){
+          if(i<tmp){ continue; }
+          imgContainer.innerHTML += `<img class="img-set" id="${i}" src="${baseurl}bucket/media/?key=${imageKeys[i]}" alt="imgset"/>`;
+        }
       })
       .catch((err) => {
         console.error(err);
@@ -79,28 +82,30 @@ uploadImgBtn.addEventListener("change", async function() {
   }
 });
 
-document.querySelector(".btn-set-img").onclick = async function (e) {
-  console.log('xbtn')
+//이미지 요소 및 키 제거
+imgContainer.onclick = async function (e) {
+  let imgSet = document.querySelectorAll(".img-set");
+  console.log(imgSet);
+  for(let i=0; i<imgSet.length; i++){
+    imgSet[i].onclick = function (e) {
+        console.log(e.target.id);
+        imageKeys.splice(Number(e.target.id), 1);
+        document.getElementById(e.target.id).remove();
+        // console.log(imgSet);
+        console.log(imageKeys);
+    }
+  }
 }
 
-
-
-
 document.querySelector(".post_btn").onclick = async function (e) {
-  
-  console.log(textArea.value);
-
   let body = {
-    nickname: profileNickname.value,
-    description: profileIntroduce.value,
+    body: textArea.value,
+    image_keys: imageKeys,
   };
-  if (profileImg != null) {
-    body.image_key = imageKey;
-  }
-  console.log('body: ' + body);
+  console.log(body);
 
-  await fetch(`${baseurl}accle/`, {
-    method: "PUT",
+  await fetch(`${baseurl}post/`, {
+    method: "POST",
     headers: {
       'Content-Type': 'application/json',
       Authorization: `bearer ${idtoken}`,
@@ -108,8 +113,11 @@ document.querySelector(".post_btn").onclick = async function (e) {
     body: JSON.stringify(body),
   })
     .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
       console.log(res);
-      window.location.href="./homepage.html"
+      window.location.href=`./post_pk.html`
     })
     .catch((err) => {
       console.error(err);
