35d905ca312623736fa6f2949d89612daabd4385
JayG-5 <jg55552758@gmail.com>
2023-09-03 02:04:40 +0900

    Feat(Module): API Module as Dio

---
+++ js/services/api/chat.js
@@ -0,0 +1,16 @@
+import { Dio } from "../../utils/dio.js";
+import { baseURL } from "../api.js";
+
+class ChatAPI {
+  constructor() {
+    this.dio = new Dio();
+  }
+
+  async create(nickname) {
+    try {
+      return await this.dio.post(baseURL.chat.create, { nickname: nickname });
+    } catch (error) {
+      return error;
+    }
+  }
+}
