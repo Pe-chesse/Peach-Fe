db8f0aabc9dbb4f66e4a81f536344b1fd3ea628c
Deeklming <deeklming6850@gmail.com>
2023-09-02 14:32:26 +0900

    feat(post): post second set

--- js/post_pk.js
+++ js/post_pk.js
@@ -19,39 +19,40 @@ let myInfo;
 let postContent;
 
 //내정보
-// onAuthStateChanged(auth, async (user)=>{
-//     if(user){
-//         idtoken = await user.getIdToken();
+onAuthStateChanged(auth, async (user)=>{
+    if(user){
+        idtoken = await user.getIdToken();
 
-//         const contentPost = async ()=>{
-//         const result = await fetch(`${baseurl}account/verify/`, {
-//             method : "GET",
-//             headers : {
-//                 Authorization: `bearer ${idtoken}`,
-//             }
-//         })
-//         .then((res)=>{
-//             return res.json();
-//         })
-//         .then((res)=>{
-//             myInfo = res;
-//             document.querySelector("#peach-user").src = myInfo.image_url;
-//             document.querySelector("#posting-user").textContent = myInfo.nickname;
-//             console.log(res);
-//         })
-//         .catch((err)=>{
-//             console.error(err);
-//         })
-//         }
-//         contentPost();
-//     }
-// })
+        const contentPost = async ()=>{
+        const result = await fetch(`${baseurl}account/verify/`, {
+            method : "GET",
+            headers : {
+                Authorization: `bearer ${idtoken}`,
+            }
+        })
+        .then((res)=>{
+            return res.json();
+        })
+        .then((res)=>{
+            myInfo = res;
+            document.querySelector(".comment-write-area-user").src = myInfo.image_url;
+            console.log(res);
+        })
+        .catch((err)=>{
+            console.error(err);
+        })
+        }
+        contentPost();
+    }
+})
 
-const imgArea = document.querySelector(".img-area");
+
 let writePostId = sessionStorage.getItem("write_post_id");
 sessionStorage.removeItem("write_post_id");
 // console.log(writePostId)
-writePostId = 25;
+if(writePostId === null){
+    writePostId = 25;
+}
 
 //포스트정보
 (async function() {
@@ -68,18 +69,18 @@ writePostId = 25;
     .then((res) => {
         console.log(res);
         postContent = res;
-        document.querySelector("#peach-user").src = postContent.user.image_url;
-        document.querySelector("#posting-user").textContent = postContent.user.nickname;
-        document.querySelector("#main-text").innerHTML = `<p>${postContent.body}</p>`;
+        document.querySelector(".post-user-img").src = postContent.user.image_url;
+        document.querySelector(".post-content-info-nick").textContent = postContent.user.nickname;
         const updatedTime = new Date(postContent.updated_at);
         const nowTime = new Date();
         const diffTime = nowTime.getTime() - updatedTime.getTime();
         let viewTime = `${Math.floor(diffTime/(60*60*1000))}시간 전`;
-        document.querySelector("#update-time").textContent = viewTime;
+        document.querySelector(".post-content-info-time").textContent = viewTime;
+        document.querySelector(".post-content-text").innerHTML = `<p>${postContent.body}</p>`;
         for(let i=0; i<postContent.image_url.length; i++){
-            imgArea.innerHTML += `<img class="posting-img" src="${postContent.image_url[i]}" alt="posting-img"/>`;
+            document.querySelector(".post-content-img").innerHTML += `<img class="post-img-list" src="${postContent.image_url[i]}" alt="post-img-list"/>`;
         }
-        document.querySelector("#heart-cnt").textContent = postContent.like_length;
+        document.querySelector(".like-count").textContent = postContent.like_length;
     })
     .catch((err) => {
         console.error(err);
@@ -87,14 +88,13 @@ writePostId = 25;
 })();
 
 
+// const commentInput = document.getElementById('comment-input');
+// const commentingButton = document.getElementById('commenting');
 
-const commentInput = document.getElementById('comment-input');
-const commentingButton = document.getElementById('commenting');
-
-commentInput.addEventListener('input', function () {
-    if (commentInput.value.trim() !== '') {
-        commentingButton.style.color = '#FFBFBF';
-    } else {
-        commentingButton.style.color = '#C4C4C4';
-    }
-});
+// commentInput.addEventListener('input', function () {
+//     if (commentInput.value.trim() !== '') {
+//         commentingButton.style.color = '#FFBFBF';
+//     } else {
+//         commentingButton.style.color = '#C4C4C4';
+//     }
+// });
