077ec5902b3cfb0aee7959bb4358e9ab8a88c5b2
Seungil Jung <jungsi1217@naver.com>
2023-09-02 20:10:31 +0900

    fix(api): fix follow api

--- js/followapi.js
+++ js/followapi.js
@@ -20,21 +20,31 @@ function checkFollow(res){
             console.log(res)
             // res = 내가 팔로잉 한 사람들
             // relInfo = 상대방 닉네임
-            if(personalInfo.nickname !== relInfo.user.nickname){
+            // if(personalInfo.nickname !== relInfo.user.nickname){
+            // let followbtn = document.querySelector('.talk-icon').nextElementSibling
+            // let count = res.length > 0 ? res.length : 1
+            // for(let i = 0; i < count; i++){
+            //     if(res.length == 0 || res[i].nickname.search(relInfo.user.nickname) == -1){
+            //         followbtn.innerHTML = `<button onclick=followClick()>팔로우</button>`
+            //         followbtn.classList.remove('unfollowing')
+            //         followbtn.classList.add('follow-btn')
+            //     }
+            //     else{
+            //         followbtn.innerHTML = `<button onclick =followClick()>언팔로우</button>`
+            //         followbtn.classList.remove('follow-btn')
+            //         followbtn.classList.add('unfollowing')
+            //     }
+            // }
+            // }
             let followbtn = document.querySelector('.talk-icon').nextElementSibling
-            let count = res.length > 0 ? res.length : 1
-            for(let i = 0; i < count; i++){
-                if(res.length == 0 || res[i].nickname.search(relInfo.user.nickname) == -1){
-                    followbtn.innerHTML = `<button onclick=followClick()>팔로우</button>`
-                    followbtn.classList.remove('unfollowing')
-                    followbtn.classList.add('follow-btn')
-                }
-                else{
-                    followbtn.innerHTML = `<button onclick =followClick()>언팔로우</button>`
-                    followbtn.classList.remove('follow-btn')
-                    followbtn.classList.add('unfollowing')
-                }
-            }
+            if(!res.filter((e)=>e.email == relInfo.user.email).length){
+                followbtn.innerHTML = `<button onclick=followClick()>팔로우</button>`
+                followbtn.classList.remove('unfollowing')
+                followbtn.classList.add('follow-btn')
+            }else{
+                followbtn.innerHTML = `<button onclick =followClick()>언팔로우</button>`
+                followbtn.classList.remove('follow-btn')
+                followbtn.classList.add('unfollowing')
             }
         })
         .catch((err)=>{
