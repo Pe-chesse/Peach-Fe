39d23289cc5dd270f4c670d98fa44ffc6cea1c18
JayG-5 <jg55552758@gmail.com>
2023-09-03 13:39:55 +0900

    feat(user): feat state management

--- js/basic.js
+++ js/basic.js
@@ -1,34 +1,44 @@
-// tab-menu
-const tabMenuElements = document.querySelectorAll('.tab-menu-elements li');
+import oAuth from "./providers/oauth.js";
+import { userInfo } from "./providers/oauth.js";
+import User from "./models/user.js";
 
-tabMenuElements.forEach(element => {
-    element.addEventListener('click', () => {
-        tabMenuElements.forEach(otherElement => {
-            otherElement.classList.remove('on');
-        });
-        element.classList.add('on');
+async function basic() {
+  await oAuth();
+  if (userInfo) {
+    document
+      .querySelector(".tab-menu-profile a")
+      .setAttribute("href", `./my_profile.html?nickname=${userInfo.nickname}`);
+  }
+  const tabMenuElements = document.querySelectorAll(".tab-menu-elements li");
+
+  tabMenuElements.forEach((element) => {
+    element.addEventListener("click", () => {
+      console.log(element);
+      tabMenuElements.forEach((otherElement) => {
+        otherElement.classList.remove("on");
+      });
+      element.classList.add("on");
     });
-});
-
-function tabProfile(){
-    const user = JSON.parse(sessionStorage.user)
-    const idtoken = user.stsTokenManager.accessToken
-    const baseurl = 'http://3.37.239.49/'
-    const findProfile = async()=>{
-        const result = await fetch(`${baseurl}api/v1/account/verify/`,{
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${idtoken}`
-            }
-        })
-        .then((res)=>{
-            return res.json()
-        })
-        .then((res)=>{
-            const nickname = res.nickname
-            document.querySelector('.tab-menu-profile a').setAttribute('href',`./my_profile.html?nickname=${nickname}`) 
-        })
-    }
-    findProfile()
+  });
 }
-tabProfile()
+basic();
+
+// function tabProfile() {
+//   const findProfile = async () => {
+//     const result = await fetch(`${baseurl}api/v1/account/verify/`, {
+//       method: "GET",
+//       headers: {
+//         Authorization: `Bearer ${idtoken}`,
+//       },
+//     })
+//       .then((res) => {
+//         return res.json();
+//       })
+//       .then((res) => {
+//         const nickname = res.nickname;
+//
+//       });
+//   };
+//   findProfile();
+// }
+// tabProfile();
